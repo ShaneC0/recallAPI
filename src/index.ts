676538dd9ba880ed10ddd489640cb0267f72f223
ShaneC0 <connors.shane@hotmail.com>
2020-10-08 10:22:42 -0400

    update cors

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ const main = async () => {
   apolloServer.applyMiddleware({
     app,
     cors: {
-      origin: "localhost:3000",
+      origin: "http://localhost:3000",
       credentials: true,
     },
   });
